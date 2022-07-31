1e94ef211d7dc80a8b321b415812589c32c75e42
Kacper Korban <kacper.f.korban@gmail.com>
2022-07-31 19:36:19 +0200

    Add some more basic tests

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -4,11 +4,33 @@ import avocado.*
 
 class tests extends munit.FunSuite {
 
-  test("correctly expand a simple option comprehension") {
+  test("correctly expand a simple option comprehension 1") {
     val res = ado {
       for {
         a <- Some(1)
         b <- Some(2)
+      } yield a + b
+    }
+    assertEquals(res, Some(3))
+  }
+
+  test("correctly expand a simple option comprehension 2") {
+    val res = ado {
+      for {
+        a <- Some(1)
+        b <- Some(2)
+        c <- Some(3)
+      } yield a + b + c
+    }
+    assertEquals(res, Some(6))
+  }
+
+  test("correctly expand a simple option comprehension 3") {
+    val res = ado {
+      for {
+        a <- Some(1)
+        b <- Some(2)
+        c <- Some(3)
       } yield a + b
     }
     assertEquals(res, Some(3))
