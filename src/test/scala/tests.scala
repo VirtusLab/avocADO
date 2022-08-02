757268a0af46f6eaee83bfe1721c75c8b737b98c
Kacper Korban <kacper.f.korban@gmail.com>
2022-08-02 02:09:20 +0200

    Add a bunch of new tests

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -48,4 +48,50 @@ class tests extends munit.FunSuite {
     assertEquals(res, Some(7))
   }
 
+  test("correctly expand a simple option comprehension 5") {
+    val res = ado {
+      for {
+        a <- Some(1)
+        b <- Some(2)
+        c <- Some(a + b)
+        d <- Some(4)
+        e <- Some(d + 1)
+      } yield c + d + e
+    }
+    assertEquals(res, Some(12))
+  }
+
+  test("correctly expand a simple option comprehension 6") {
+    val res = ado {
+      for {
+        a <- Some(1)
+        b <- Some(2)
+        c <- Some(a + b)
+        d <- None.asInstanceOf[Option[Int]]
+      } yield c + d
+    }
+    assertEquals(res, None)
+  }
+
+  test("correctly expand a simple option comprehension 7") {
+    val res = ado {
+      for {
+        a <- Some(1)
+        aa <- None
+        b <- Some(2)
+        c <- Some(a + b)
+      } yield c + 1
+    }
+    assertEquals(res, None)
+  }
+
+  test("correctly expand a simple option comprehension 8") {
+    val res = ado {
+      for {
+        a <- Some(1)
+      } yield a
+    }
+    assertEquals(res, Some(1))
+  }
+
 }
